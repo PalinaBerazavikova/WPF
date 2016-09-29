8028116fba7561a15ae1757ad49a4e9bce21df3d
PalinaBerazavikova <Palin_Berazavikova@epam.com>
2016-09-29 22:17:00 +0300

    1

---
+++ Collections/Collections/SortedSetPerfomance.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class SortedSetPerfomance
+    {
+        public static SortedSet<int> TestSortedSet { get; set; } = new SortedSet<int>();
+
+        public static void WriteToFile()
+        {
+            string output = string.Empty;
+            output = $"{output}SortedSet adding time (1 000 000 elements) {AddingTime()}{Environment.NewLine}";
+            output = $"{output}SortedSet reading time (1 000 000 elements) {ReadingTime()}{Environment.NewLine}";
+            output = $"{output}SortedSet searching time (1 000 000 elements) {SearchingTime()}{Environment.NewLine}";
+            output = $"{output}SortedSet removing time (10 000 elements) {RemovingTime()}{Environment.NewLine}";
+            using (StreamWriter sw = File.AppendText("CollectionsPerfomance.txt"))
+            {
+                sw.WriteLine(output);
+            }
+        }
+
+        public static string AddingTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                TestSortedSet.Add(i);
+            }
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:0000}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+            return elapsedTime;
+        }
+
+        public static string ReadingTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            int y;
+            stopWatch.Start();
+            foreach (int i in TestSortedSet)
+            {
+                y = i;
+            }
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:0000}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+            return elapsedTime;
+        }
+
+        public static string SearchingTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            int y;
+            stopWatch.Start();
+            foreach (int i in TestSortedSet)
+            {
+                if (i == 12345)
+                {
+                    y = i;
+                }
+            }
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:0000}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+            return elapsedTime;
+        }
+
+        public static string RemovingTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                TestSortedSet.Remove(i);
+            }
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00000}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+            return elapsedTime;
+        }
+    }
+}
---
+++ Collections/Collections/StackPerfomance.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class StackPerfomance
+    {
+        public static Stack<int> TestStack { get; set; } = new Stack<int>();
+
+        public static void WriteToFile()
+        {
+            string output = string.Empty;
+            output = $"{output}Stack adding time (1 000 000 elements) {AddingTime()}{Environment.NewLine}";
+            output = $"{output}Stack reading time (1 000 000 elements) {ReadingTime()}{Environment.NewLine}";
+            output = $"{output}Stack searching time (1 000 000 elements) {SearchingTime()}{Environment.NewLine}";
+            output = $"{output}Stack removing time (100 000 elements) {RemovingTime()}{Environment.NewLine}";
+            using (StreamWriter sw = File.AppendText("CollectionsPerfomance.txt"))
+            {
+                sw.WriteLine(output);
+            }
+        }
+
+        public static string AddingTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                TestStack.Push(i);
+            }
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:0000}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+            return elapsedTime;
+        }
+
+        public static string ReadingTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            int y;
+            stopWatch.Start();
+            foreach (int i in TestStack)
+            {
+                y = i;
+            }
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:0000}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+            return elapsedTime;
+        }
+
+        public static string SearchingTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            int y;
+            stopWatch.Start();
+            foreach (int i in TestStack)
+            {
+                if (i == 12345)
+                {
+                    y = i;
+                }
+            }
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:0000}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+            return elapsedTime;
+        }
+
+        public static string RemovingTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                TestStack.Pop();
+            }
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00000}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+            return elapsedTime;
+        }
+    }
+}
+
